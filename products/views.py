5d81bbd2fe5b05065da9673c976ea447ce8e735d
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-10-12 19:34:50 +0300

    solved the task

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+
+
+def get_home(request):
+    return HttpResponse("HELLO")
+
+
+def get_product(request):
+    product = Product.objects.get(id=1)
+    return HttpResponse(f" your order is {product}")
